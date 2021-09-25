a4a830d1ee4d1e2274c12b9834a3eee4daae5e13
cdp98 <pintodanielcarlos@gmail.com>
2021-09-25 14:58:16 -0300

    Creating migrations and models

---
+++ app/Models/SaleProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SaleProduct extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'id',
+        'sale_id',
+        'product_id'
+    ];
+}
